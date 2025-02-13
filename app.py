f5d911489326e41e3c75f549b5eb9b9dc3296104
StruggleWuggle <brianqre@gmail.com>
2025-02-13 05:28:18 -0800

    Added dynamic table generation from data

--- app.py
+++ app.py
@@ -58,27 +58,31 @@ people_from_app_py = [
 def root():
     return render_template("main.j2", people=people_from_app_py)
 
-@app.route('/bsg-people')
-def bsg_people():
+@app.route('/app-users')
+def app_users():
     # Write the query and save it to a variable
-    query = "SELECT * FROM bsg_people;"
+    query = "SELECT * FROM app_users;"
 
     # The way the interface between MySQL and Flask works is by using an
     # object called a cursor. Think of it as the object that acts as the
     # person typing commands directly into the MySQL command line and
     # reading them back to you when it gets results
     cursor = db.execute_query(db_connection=db_connection, query=query)
+    results = cursor.fetchall()
+    return render_template("table_template.j2", data=results)
 
-    # The cursor.fetchall() function tells the cursor object to return all
-    # the results from the previously executed
-    #
-    # The json.dumps() function simply converts the dictionary that was
-    # returned by the fetchall() call to JSON so we can display it on the
-    # page.
-    results = json.dumps(cursor.fetchall())
+@app.route('/anime', methods=["POST", "GET"])
+def anime():
+    if request.method == "GET":
+        query = "SELECT * FROM anime;"
+        cursor = db.execute_query(db_connection=db_connection, query=query)
+        results = cursor.fetchall()
+        return render_template("table_template.j2", data=results)
+    
+    if request.method == "POST":
 
-    # Sends the results back to the web browser.
-    return results
+        return redirect("/anime")
+    
 
 # Listener
 if __name__ == "__main__":
